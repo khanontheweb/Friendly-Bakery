f13d241207851bba4912faa857a6e956e41c0ef8
Momo Khan <snillmomo@gmail.com>
2018-12-05 16:10:00 -0500

    Added hashes for each of the types of baked goods. Changed navbar buttons.

--- baked_goods.rb
+++ baked_goods.rb
@@ -1,8 +1,30 @@
 class BakedGoods
     attr_accessor :name, :description, :price, :type
+    @@cookies = Hash.new
+    @muffins = Hash.new
+    @cakes = Hash.new
     def initialize(name, description, price, type)
-        @name = "#{name} #{type}"
-        @description = description
-        @price = price
+        if type.downcase == 'cookie'
+            @@cookies[name.to_sym] = {
+                :description => description,
+                :price => price,
+                :type => type
+            }
+        elsif type.downcase == 'muffin'
+            @@muffin[name.to_sym] = {
+                :description => description,
+                :price => price,
+                :type => type
+            }
+        elsif type.downcase == 'cake'
+            @@cakes[name.to_sym] = {
+                :description => description,
+                :price => price,
+                :type => type
+            }
+        else 
+            puts "This not a good"
+        end
+        
     end
 end
--- server.rb
+++ server.rb
@@ -3,7 +3,6 @@ require './baked_goods'
 
 get '/' do 
     @cookie = BakedGoods.new("Chocolate Chip", "daddy", "9.99", "Cookie")
-    
     erb :home
 
 end
